ccbbafd20c27a21f79401756ae533e987fae593b
Alt0252 <will0527@gmail.com>
2013-07-07 21:19:43 +0000

    Testing

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync('index.html');
-var message = buf.toString();
+//var buf = fs.readFileSync('index.html');
+//var message = buf.toString();
 
 app.get('/', function(request, response) {
   response.send('testing');
