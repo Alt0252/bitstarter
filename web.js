1758e8b90534beaf5e4e6b22aa5b22e48f18ff6a
Alt0252 <will0527@gmail.com>
2013-07-07 20:11:30 +0000

    Corrected input

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var message = fs.readFileSync(index.html)
+var message = fs.readFileSync('index.html')
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
